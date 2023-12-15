ff13fe6577e7238170eb51d81988f68c2ea3872e
Kevin Chua <Kevin_chua3@hotmail.com>
2023-12-15 10:22:13 +0000

    Trying to add map

--- industry-project-1/data.js
+++
@@ -1,8 +0,0 @@
-const mapObject = L.map("mapContainer");
-
-mapObject.setView([1.3521, 103.8198], 13);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(mapObject);
---
+++ industry-project-1/script.js
@@ -0,0 +1,9 @@
+const mapObject = L.map("mapContainer");
+
+mapObject.setView([1.3521, 103.8198], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapObject);
+
